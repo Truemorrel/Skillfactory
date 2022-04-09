e3b8fbc15bf5483c5c86da0ed84b01ef9f43fb3b
Sergey Bednyakov <sbednyakov@gmail.com>
2022-04-10 00:12:01 +0300

    Init Skillfactory

---
+++ FirstApplication/AfterFirstApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AfterFirstApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, сколько вам лет?", name);
+            string age = Console.ReadLine();
+            Console.WriteLine("{0}, как называется ваша профессия?", name);
+            string job_title = Console.ReadLine();
+            Console.WriteLine("{0}, сколько вы работаете в профессии {1}?",
+                name, job_title);
+            string job_term = Console.ReadLine();
+            Console.WriteLine("Вас зовут {0}, вам {1} лет, вы {2} лет работаете {3}ом.",
+                name, age, job_term, job_title);
+        }
+    }
+}
---
+++ FirstApplication/FirstApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, {0}", name);
+        }
+    }
+}
